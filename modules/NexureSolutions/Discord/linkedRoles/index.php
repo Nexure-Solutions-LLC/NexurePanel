c28b6da855e380045b6e147c0b486b13c3ebb178
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-02-01 08:43:44 +0000

    [BUGFIX] Unset the referral url once they have started the oauth flow.

--- modules/NexureSolutions/Discord/linkedRoles/index.php
+++ modules/NexureSolutions/Discord/linkedRoles/index.php
@@ -5,7 +5,6 @@
 
     require($_SERVER["DOCUMENT_ROOT"] . "/modules/NexureSolutions/Utility/Backend/System/Dashboard.php");
 
-
     use Dotenv\Dotenv;
 
 
@@ -17,6 +16,7 @@
     $client_secret = $_ENV['DISCORD_CLIENT_SECRET'];
     $redirect_uri = $_ENV['DISCORD_LINKEDROLES_REDIRECT_URI'];
 
+    unset($_SESSION["referral_url"]);
 
     $auth_url = "https://discord.com/api/oauth2/authorize?client_id=$client_id&redirect_uri=" . urlencode($redirect_uri) . "&response_type=code&scope=identify%20role_connections.write";
 
