25822ac3d949513ec28cee942984d261864e5542
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-02-02 08:25:42 +0000

    [UPDATE] Updated the links from caliwebdesignservices.com to nexuresolutions.com

--- modules/NexureSolutions/Utility/Backend/Login/Headers/index.php
+++ modules/NexureSolutions/Utility/Backend/Login/Headers/index.php
@@ -50,11 +50,11 @@
         -->
         <html lang="en-us">
             <head>
-                <script src="https://caliwebdesignservices.com/assets/js/darkmode.js" type="text/javascript"></script>
+                <script src="https://nexuresolutions.com/assets/js/v1/darkmode.js" type="text/javascript"></script>
                 <meta charset="utf-8" />
                 <meta content="width=device-width, initial-scale=1" name="viewport" />
                 <meta name="author" content="Nexure Development Team, Nick Derry, Michael Brinkley">
-                <link href="https://caliwebdesignservices.com/assets/css/2024-01-29-styling.css" rel="stylesheet" type="text/css" />
+                <link href="https://nexuresolutions.com/assets/css/v1/2024-01-29-styling.css" rel="stylesheet" type="text/css" />
                 <link href="/assets/css/login-css-2024.css" rel="stylesheet" type="text/css" />
                 <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
