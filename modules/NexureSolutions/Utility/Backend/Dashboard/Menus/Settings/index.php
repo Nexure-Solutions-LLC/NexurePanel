9d4bd71de0e31ebbbe4d8ba0f10fc502bcc8ad10
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-03-26 08:03:44 +0000

    [UPDATE] Added the ability to view logs for the system when there is a generic error. This can help diagnose card declines, payment system errors, generic php programming errors, etc. You can provide these errors to Nexure for more information and ways to resolve it. This update also fixes several UI bugs with the headers in Domain Checker, Settings, Account Center and more.

---
+++ modules/NexureSolutions/Utility/Backend/Dashboard/Menus/Settings/index.php
@@ -0,0 +1,34 @@
+<?php
+
+    function isActive($page) {
+
+        $currentPage = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        return ($currentPage === $page) ? 'active' : '';
+
+    }
+
+?>
+
+<aside class="caliweb-sidebar">
+    <ul class="sidebar-list-linked">
+        <a href="/dashboard/administration/settings/" class="sidebar-link-a">
+            <li class="sidebar-link <?= isActive('/dashboard/administration/settings/') ?>">General</li>
+        </a>
+        <a href="/dashboard/administration/settings/logs/" class="sidebar-link-a">
+            <li class="sidebar-link <?= isActive('/dashboard/administration/settings/logs/') ?>">Logs</li>
+        </a>
+        <a href="/dashboard/administration/settings/ipBaning" class="sidebar-link-a">
+            <li class="sidebar-link <?= isActive('/dashboard/administration/settings/ipBaning/') ?>">IP Banning</li>
+        </a>
+        <a href="/licensing/" class="sidebar-link-a">
+            <li class="sidebar-link <?= isActive('/licensing') ?>">Licensing</li>
+        </a>
+        <a href="/dashboard/administration/settings/update" class="sidebar-link-a">
+            <li class="sidebar-link <?= isActive('/dashboard/administration/settings/update/') ?>">Updates</li>
+        </a>
+        <a href="/dashboard/administration/settings/about" class="sidebar-link-a">
+            <li class="sidebar-link <?= isActive('/dashboard/administration/settings/about/') ?>">About Nexure Panel</li>
+        </a>
+    </ul>
+</aside>
