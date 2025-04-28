22cfdc4f1de068e819d96863548983aa2b0f1ece
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-04-28 21:16:28 +0000

    [UPDATE] Redirection on account status changes and shows the corresponding dashboard for the user. Improved the error handling in the VariableDefinitions namespace and created better handling on dates to allow for null values. Fixed the Sentry error logging as it was not being used correctly. Cleaned up the code and function calls from the original.

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -1,9 +1,34 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
     
     include($_SERVER["DOCUMENT_ROOT"]."/Modules/NexureSolutions/Dashboard/Backend/index.php");
+
+    $VariableDefinitionsG = new \NexureSolutions\Generic\VariableDefinitions();
+
+    $VariableDefinitionsG->GatherOnlineAccessInformation($con, $nexureid);
+
+    $upperAccessType = $VariableDefinitionsG->accessType;
+
+    $lowerAccessType = strtolower($upperAccessType);
+
+    switch ($lowerAccessType) {
+        case "authorized user":
+            header("location:/Dashboard/AuthorizedUser");
+            break;
+        case "partner":
+            header("location:/Dashboard/Partners");
+            break;
+        case "administrator":
+            header("location:/Dashboard/Administration");
+            break;
+        case 'personal':
+            header("location:/Dashboard/Personal");
+            break;
+        case 'business':
+            header("location:/Dashboard/Business");
+            break;
+        default:
+            header("Location: /ErrorHandling/ErrorPages/GenericError");
+            break;
+    }
 
 ?>
