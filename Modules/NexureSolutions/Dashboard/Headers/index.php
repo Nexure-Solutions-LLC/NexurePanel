8176d5944c01642be180680d2b4c25522a48145c
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-05-14 02:52:10 +0000

    [UPDATE] Built the base line system files and the customer front end.

---
+++ Modules/NexureSolutions/Dashboard/Headers/index.php
@@ -0,0 +1,148 @@
+<?php
+
+    include($_SERVER["DOCUMENT_ROOT"]."/Modules/NexureSolutions/Dashboard/Backend/index.php");
+
+?>
+
+    <html>
+
+        <header>
+            <meta charset="utf-8" />
+            <meta name="author" content="Nexure Development Team">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta property="og:image" content="https://nexuresolutions.com/assets/img/opengraphimage/opengraphimage.webp" />
+            <meta property="og:type" content="website" />
+            <meta content="summary_large_image" name="twitter:card" />
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta content="NexureSolutions" name="generator" />
+            <meta name="languageCode" content="en"/>
+            <meta name="countryCode" content="us"/>
+            <meta name="focusArea" content="No Contact Module"/>
+            <link rel="canonical" href="https://nexuresolutions.com/"/>
+            <link href="https://nexuresolutions.com/assets/css/2024-11-04-styling.css" rel="stylesheet" type="text/css" />
+            <link rel="stylesheet" href="https://nexuresolutions.com/assets/css/2024-11-08-fonts.css">
+            <link rel="stylesheet" href="/Assets/css/2025-dashboard-css-v2.css" />
+            <link rel="apple-touch-icon" sizes="180x180" href="https://nexuresolutions.com/assets/img/favicon/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="https://nexuresolutions.com/assets/img/favicon/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="https://nexuresolutions.com/assets/img/favicon/favicon-16x16.png">
+            <link rel="manifest" href="https://nexuresolutions.com/assets/img/favicon/site.webmanifest">
+            <script type="text/javascript">   
+                window.antiFlicker = {
+                    active: true,
+                    timeout: 3000
+                }           
+            </script>
+            <script src="https://nexuresolutions.com/assets/js/darkmode.js" type="text/javascript"></script>
+            <script type="text/javascript">
+                var languageCode = document.getElementsByName('languageCode')[0].content;
+                var countryCode = document.getElementsByName('countryCode')[0].content;
+                var focusArea = document.getElementsByName('focusArea')[0].content;
+                /* Define digital data object based on _appInfo object */
+                window.digitalData = {
+                    page: {
+                        category: {
+                            primaryCategory: '',
+                        },
+                        pageInfo: {
+                            language: languageCode + '-' + countryCode,
+                            NexureSolutions: {
+                                siteID: 'MarketingAEM',
+                                country: countryCode,
+                                messaging: {
+                                    routing: {
+                                        focusArea: focusArea,
+                                        languageCode: languageCode,
+                                        regionCode: countryCode
+                                    },
+                                    translation: {
+                                        languageCode: languageCode,
+                                        regionCode: countryCode
+                                    }
+                                },
+                                sections: 0,
+                                patterns: 0
+                            }
+                        }
+                    }
+                };
+            </script>
+        </header>
+
+        <body>
+
+            <div class="nexure-header" id="nexure-header-js">
+                <div class="background-darker-300">
+                    <div class="container nexure-container nexure-nav-container">
+                        <div class="display-flex align-center">
+                            <div class="nexure-branding">
+                                <a href="https://nexuresolutions.com/">
+                                    <img src="https://nexuresolutions.com/assets/img/logos/NexureWideLogoBlack.svg" loading="lazy" alt="Nexure Solutions Logo" class="nexure-logo light-mode">
+                                    <img src="https://nexuresolutions.com/assets/img/logos/NexureWideLogoWhite.svg" loading="lazy" alt="Nexure Solutions Logo" class="nexure-logo dark-mode">
+                                </a>
+                            </div>
+                            <div class="nexure-navbar-menu" id="nexure-navbar-js"></div>
+                        </div>
+                        <div class="display-flex align-center">
+                            <a href="/Dashboard/Settings/Account"> 
+                                <img src="/Assets/img/ProfileImages/Default.png" class="image-fluid profile-image" />
+                            </a>
+                            <span class="toggle-container">
+                                <span class="lnr lnr-sun" class="toggle-input" id="lightModeIcon"></span>
+                                <span class="lnr lnr-moon"  class="toggle-input" id="darkModeIcon"></span>
+                            </span>
+                            <button style="background-color:transparent; border:none; outline:0;" href="javascript:void(0);" class="nexure-menu-icon" aria-label="Mobile Menu" onclick="responsiveMenu()">
+                                <img src="https://nexuresolutions.com/assets/img/systemicons/menu.svg" loading="lazy" width="24" alt="" class="menu-icon">
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-top-grey-300">
+                    <div class="container display-flex align-center justify-content-space-between">
+                        <nav class="nexure-navbar-menu display-flex align-center" id="nexure-navbar-js">
+                            <p class="no-margin no-padding" style="padding-right:20px; padding-top:2px; font-weight:500;">Dashboard</p>
+                            <li class="nav-links"><a href="/">Dashboard</a></li>
+                            <li class="nav-links"><a href="/">Open an account</a></li>
+                            <li class="nav-links"><a href="/">Billing</a></li>
+                            <li class="nav-links"><a href="/">Support Center</a></li>
+                            <li class="nav-links"><a href="/">Access and Security</a></li>
+                            <li class="nav-links"><a href="/">Service Information</a></li>
+                        </nav>
+                        <div class="systemLoads display-flex align-center">
+                            <p class="font-14px">
+                                <?php
+
+                                    $loads = sys_getloadavg();
+
+                                    $rounded_loads = array_map(function ($load) {
+
+                                        return number_format($load, 2);
+                                    }, $loads);
+
+                                    echo "System Loads: " . implode(", ", $rounded_loads);
+
+                                ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+<?php 
+    
+    if (isset($_SESSION["lang"])) {
+
+        if (!file_exists($_SERVER["DOCUMENT_ROOT"].'/Language/'.$_SESSION["lang"].'.php')) {
+
+            $_SESSION["lang"] = 'EN_US';
+
+        }
+
+        include($_SERVER["DOCUMENT_ROOT"].'/Language/'.$_SESSION["lang"].'.php');
+
+    } else {
+
+        include($_SERVER["DOCUMENT_ROOT"]."/Language/EN_US.php");
+
+    }
+
+?>
