4c9b33a08fc8b92aae7c989cdc747eb4546966af
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-05-15 04:24:43 +0000

    [UPDATE] Added the quick actions links, added the folders for QuickActions, added the Onboarding UI and improved onboarding flow., This is up to Business Information. Added a check to prevent rejected accounts from showing to the customer.

---
+++ Register/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    include($_SERVER["DOCUMENT_ROOT"]."/Modules/NexureSolutions/Login/Headers/index.php");
+
+?>
+
+<section class="section" style="padding-top:0%; padding-left:10%;">
+    <div class="container nexure-container">
+        <div style="display:flex; align-items:center;">
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+
+    include($_SERVER["DOCUMENT_ROOT"]."/Modules/NexureSolutions/Login/Footers/index.php"); 
+
+?>
