99b8e5b301a530cbf2eb3da09fc360d8f4cf0d26
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-05-16 10:11:36 +0000

    [UPDATE] Created more files and added theme support.

--- Modules/NexureSolutions/Dashboard/Footers/index.php
+++ Modules/NexureSolutions/Dashboard/Footers/index.php
@@ -1,3 +1,12 @@
+    <?php 
+            
+        if ($VariableDefinitionHandler->panelTheme != "NexureDefault") {
+            
+            echo '<script src="/Themes/'.$VariableDefinitionHandler->panelTheme.'/Assets/js/index.js"" type="text/javascript"></script>';
+
+        }
+
+    ?>
     <script type="text/javascript"  src="/Assets/js/Dashboard.js"></script>
     <script src="https://nexuresolutions.com/assets/js/index.js" type="text/javascript"></script>
     </body>
