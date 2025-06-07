ba54e541b32325b87aa6d89aab773b28185bdde2
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-06-07 13:27:35 +0000

    Added commands - show / add / remove owners

--- Modules/Discord/Bot/cogs/developer_commands.py
+++ Modules/Discord/Bot/cogs/developer_commands.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+
 from utils.utils import NexureContext
+from utils.constants import NexureConstants
 
 class DeveloperCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.nexure_constants = NexureConstants()
 
     @commands.command()
     @commands.is_owner()
@@ -16,6 +19,39 @@ class DeveloperCommands(commands.Cog):
         else:
             raise commands.BadArgument()
         await ctx.send("Synced!")
+
+    @commands.command()
+    @commands.is_owner()
+    async def add_owner(self, ctx: NexureContext, user: discord.User):
+        await ctx.defer()
+        try:
+            embed = discord.Embed(title='Success!', description=f'{self.nexure_constants.emojis()['success']} Added user to bypass.', color=self.nexure_constants.colour())
+            await ctx.bot.database.execute(f'UPDATE {self.nexure_constants.sql_users()} SET botAuth = %s WHERE oAuthID = %s;', 1, user.id)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            raise Exception('Failed to update database')
+    
+    @commands.command()
+    @commands.is_owner()
+    async def remove_owner(self, ctx: NexureContext, user: discord.User):
+        await ctx.defer()
+        try:
+            embed = discord.Embed(title='Success!', description=f'{self.nexure_constants.emojis()['success']} Removed user from bypass.', color=self.nexure_constants.colour())
+            await ctx.bot.database.execute(f'UPDATE {self.nexure_constants.sql_users()} SET botAuth = %s WHERE oAuthID = %s;', 0, user.id)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            raise Exception('Failed to update database')
+
+    @commands.command()
+    @commands.is_owner()
+    async def show_owners(self, ctx: NexureContext, user: discord.User):
+        res = await ctx.bot.database.fetch(f'SELECT oAuthID FROM {self.nexure_constants.sql_users()} WHERE botAuth = %s', 1)
+        embed = discord.Embed(title='Authorised Users', description=' ', colour=self.nexure_constants.colour())
+
+        for user in res[0]:
+            embed.description += f'<@{user}>\n'
+
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(DeveloperCommands(bot))
