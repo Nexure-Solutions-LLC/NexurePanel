6481e72ea9fc54f46ed1b368461551681e4a2f90
Treyten Sanders <uvloop@outlook.com>
2025-06-08 16:38:00 -0400

    Create keychain.py

---
+++ Modules/Discord/BotRewrite/bot/core/main/keychain.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from bot.drivers.network._essentials import HEADERS
+
+from cryptography.fernet import Fernet
+import requests
+
+
+class Keychain:
+    def __init__(self: Keychain):
+        self.__FERNET_KEY = requests.get("http://localhost:1515").text
+        self.__fernet = Fernet(self.FERNET_KEY)
+
+        for key, value in HEADERS.Authorization.items():
+            setattr(self, key, self.__fernet.decrypt(value.encode()).decode())
