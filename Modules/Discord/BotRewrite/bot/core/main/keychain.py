c729da353fd233aa5912e39e8292a1625ab25a46
Treyten Sanders <uvloop@outlook.com>
2025-06-10 09:16:35 -0400

    Update keychain.py

--- Modules/Discord/BotRewrite/bot/core/main/keychain.py
+++ Modules/Discord/BotRewrite/bot/core/main/keychain.py
@@ -8,7 +8,7 @@ import requests
 class Keychain:
     def __init__(self: Keychain):
         self.__FERNET_KEY = requests.get("http://localhost:1515").text
-        self.__fernet = Fernet(self.FERNET_KEY)
+        self.__fernet = Fernet(self.__FERNET_KEY)
 
         for key, value in HEADERS.Authorization.items():
             setattr(self, key, self.__fernet.decrypt(value.encode()).decode())
