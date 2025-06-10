61e1ead6b4890d4dfea31cd53b3f8b10d3da37f9
Treyten Sanders <uvloop@outlook.com>
2025-06-10 08:49:39 -0400

    Create __main__.py

---
+++ Modules/Discord/BotRewrite/bot/__main__.py
@@ -0,0 +1,25 @@
+
+from bot.core.main import NexureClient
+from os.path import abspath, dirname
+
+import asyncio
+import sys
+
+sys.path.append(dirname(abspath(__file__)))
+
+
+async def run():
+    async with NexureClient() as bot:
+        try:
+            await bot.start(token=bot.keychain.DISCORD_AUTH_TOKEN, reconnect=False)
+
+        finally:
+            if not bot.is_closed():
+                await bot.close()
+                
+            bot.session.close()
+            bot.logger.info("Application shut down.")
+            
+            
+assert __name__ == "__main__"
+asyncio.run(run())
