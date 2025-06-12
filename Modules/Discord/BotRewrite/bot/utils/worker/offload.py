ac97116c20aa88d26838b02c77b4f69f85568973
Treyten Sanders <uvloop@outlook.com>
2025-06-11 21:29:10 -0400

    Refactor Offload decorator to simplify imports and improve type annotations

--- Modules/Discord/BotRewrite/bot/utils/worker/offload.py
+++ Modules/Discord/BotRewrite/bot/utils/worker/offload.py
@@ -1,16 +1,16 @@
-from functools import wraps as Wraps
-from typing import Any, Callable, Dict, List, Optional
+from functools import wraps
+from typing import Any, Callable, Optional
+import cloudpickle
 
 
 def Offload(function: Callable) -> Optional[Callable]:
     try:
         from dask import GLOBAL
-    
     except ImportError:
         raise AttributeError("Dask has not been initialized through StartDask.")
-    
-    @Wraps(function)
-    async def wrapper(*args: List[Any], **kwargs: Dict[ str, Any ]) -> Any:
-        return await __import__("dask").GLOBAL.submit(function, *args, **kwargs)
-        
+
+    @wraps(function)
+    async def wrapper(*args: Any, **kwargs: Any) -> Any:
+        return await GLOBAL.submit(cloudpickle.loads(cloudpickle.dumps(function)), *args, **kwargs)
+
     return wrapper
