399a2adca23b7fb0c29e25794329c9d8408007b5
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-08-08 02:32:50 +0000

    [UPDATE] Fixed the login background to only apply to the login page using a custom class and div instead of using the body tag.

--- Login/index.php
+++ Login/index.php
@@ -95,38 +95,42 @@
 
         <!-- Login Page main content area -->
 
-        <div class="nexure-login-section">
-            <div class="nexure-login-content height-100">
-                <div class="container nexure-container">
-                    <div class="logo-area margin-bottom-30px">
-                        <img src="<?php echo $VariableDefinitionHandler->organizationSquareLogo; ?>" loading="lazy" alt="Nexure Solutions Logo" class="nexure-logo square-logo light-mode">
-                        <img src="<?php echo $VariableDefinitionHandler->organizationSquareLogo; ?>" loading="lazy" alt="Nexure Solutions Logo" class="nexure-logo square-logo dark-mode">
-                    </div>
-                    <form action="" method="POST" class="nexure-login-form" id="nexure-form-plugin">
-                        <div class="form-control margin-top-10px">
-                            <label for="nexureid" class="font-12px"><?php echo $VariableDefinitionHandler->organizationShortName; ?> ID</label><br>
-                            <input class="nexure-textbox width-100" name="nexureid" id="nexureid" type="email" placeholder="me@example.com" />
+        <div class="login-background">
+
+            <div class="nexure-login-section">
+                <div class="nexure-login-content height-100">
+                    <div class="container nexure-container">
+                        <div class="logo-area margin-bottom-30px">
+                            <img src="<?php echo $VariableDefinitionHandler->organizationSquareLogo; ?>" loading="lazy" alt="Nexure Solutions Logo" class="nexure-logo square-logo light-mode">
+                            <img src="<?php echo $VariableDefinitionHandler->organizationSquareLogo; ?>" loading="lazy" alt="Nexure Solutions Logo" class="nexure-logo square-logo dark-mode">
                         </div>
-                        <div class="form-control">
-                            <label for="password" class="font-12px"><?php echo $LANG_LOGIN_PASSWORD; ?></label><br>
-                            <input class="nexure-textbox width-100" name="password" id="password" type="password" placeholder="Super Secret Password" />
+                        <form action="" method="POST" class="nexure-login-form" id="nexure-form-plugin">
+                            <div class="form-control margin-top-10px">
+                                <label for="nexureid" class="font-12px"><?php echo $VariableDefinitionHandler->organizationShortName; ?> ID</label><br>
+                                <input class="nexure-textbox width-100" name="nexureid" id="nexureid" type="email" placeholder="me@example.com" />
+                            </div>
+                            <div class="form-control">
+                                <label for="password" class="font-12px"><?php echo $LANG_LOGIN_PASSWORD; ?></label><br>
+                                <input class="nexure-textbox width-100" name="password" id="password" type="password" placeholder="Super Secret Password" />
+                            </div>
+                            <div class="form-control margin-top-20px">
+                                <button type="submit" name="submit" class="nexure-button primary width-100"><?php echo $LANG_LOGIN_BUTTON; ?></button>
+                            </div>
+                        </form>
+                        <?php if (isset($login_error)): ?>
+                            <div class="nexure-error-box">
+                                <p class="nexure-login-sublink" style="font-weight:700; padding-top:0; margin-top:0;"><?php echo $LANG_LOGIN_AUTH_ERROR_TITLE; ?></p>
+                                <p class="nexure-login-sublink" style="font-size:12px;"><?php echo $LANG_LOGIN_AUTH_ERROR_TEXT; ?></p>
+                            </div>
+                        <?php endif; ?>
+                        <div class="after-login-area display-flex align-center justify-content-space-between margin-top-30px margin-bottom-10px padding-top-10px">
+                            <p class="margin-top-10px gray-100 font-12px">Forgot Login? <a class="brand-link font-12px" href="/ResetPassword">Reset it</a>.</a>
+                            <p class="margin-top-10px gray-100 font-12px">Not enrolled? <a class="brand-link font-12px" href="/Register">Register Now</a>.</a>
                         </div>
-                        <div class="form-control margin-top-20px">
-                            <button type="submit" name="submit" class="nexure-button primary width-100"><?php echo $LANG_LOGIN_BUTTON; ?></button>
-                        </div>
-                    </form>
-                    <?php if (isset($login_error)): ?>
-                        <div class="nexure-error-box">
-                            <p class="nexure-login-sublink" style="font-weight:700; padding-top:0; margin-top:0;"><?php echo $LANG_LOGIN_AUTH_ERROR_TITLE; ?></p>
-                            <p class="nexure-login-sublink" style="font-size:12px;"><?php echo $LANG_LOGIN_AUTH_ERROR_TEXT; ?></p>
-                        </div>
-                    <?php endif; ?>
-                    <div class="after-login-area display-flex align-center justify-content-space-between margin-top-30px margin-bottom-10px padding-top-10px">
-                        <p class="margin-top-10px gray-100 font-12px">Forgot Login? <a class="brand-link font-12px" href="/ResetPassword">Reset it</a>.</a>
-                        <p class="margin-top-10px gray-100 font-12px">Not enrolled? <a class="brand-link font-12px" href="/Register">Register Now</a>.</a>
                     </div>
                 </div>
             </div>
+        
         </div>
 
 <?php 
